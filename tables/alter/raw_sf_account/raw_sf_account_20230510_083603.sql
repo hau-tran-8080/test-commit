80b4649994014d79c8b7495491cc1de1c203c3ba
Hau Tran <hau.tran.8080@gmail.com>
2023-05-11 09:00:20 +0700

    Delete tables directory

--- tables/alter/raw_sf_account/raw_sf_account_20230510_083603.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS dw_raw.raw_sf_account (
-    id character varying(255) ENCODE lzo,
-    isdeleted character varying(10) ENCODE lzo,
-    masterrecordid character varying(255) ENCODE lzo,
-    name character varying(255) ENCODE lzo,
-    type character varying(255) ENCODE bytedict,
-) DISTSTYLE AUTO;
-
-CALL public.sp_add_table_column('dw_raw', 'raw_sf_caccount', 'channel__c', 'VARCHAR (255)');
